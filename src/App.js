b8703e73aa7c1535b5580e434fde999911401ba5
shippusharma <shippusharma00@gmail.com>
2022-05-25 20:01:43 +0530

    add useConnect hook

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
-
+import { Routes, Route } from 'react-router-dom';
+import Header from './containers/Header';
+import ProductListing from './containers/ProductListing';
+import productdetails from './containers/productdetails';
+// import productComponent from './containers/productComponent';
 function App() {
   return (
     <div className='App'>
-      <h1>Redux app</h1>
+      <Header />
+      <Routes>
+        <Route path='/' exact component={ProductListing} />
+        <Route path='/product/:productId' exact component={productdetails} />
+        {/* <Route path='/' exact component={productComponent} /> */}
+      </Routes>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import './index.css';
 import { Provider } from 'react-redux';
 import store from './store';
 import App from './App';
-
+import { BrowserRouter } from 'react-router-dom';
+// https://youtu.be/0W6i5LYKCSI
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
